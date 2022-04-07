19364f1bdb57e6a308620110afc8920ca2c8f560
Ali-Abdelkader118 <86664989+Ali-Abdelkader118@users.noreply.github.com>
2022-04-07 15:59:13 +0100

    Add files via upload

---
+++ PHP-Assignment-1.php
@@ -0,0 +1,45 @@
+<?php
+/*
+=========================
+== My First Assignment ==
+=========================
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset=<?=' UTF-8'?>>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> <?php 
+    echo "My First PHP Page";
+    ?> </title>
+</head>
+<body>
+    <div><?php print'We Love'; ?> </div>
+    <div><?php echo 'Elzero Channel'; ?></div>
+</body>
+</html>
+
+<?php
+#Second-Assignment
+
+//echo 'Prevent Me From Running Please';
+#echo 'Prevent Me From Running Please';
+/*echo 'Prevent Me From Running Please';*/
+
+#Third-Assignment
+/* My Application
+Version 1.0
+Created By Elzero*/
+
+
+#Fourth-Assignment
+
+// ## First Comment
+# // # Second Comment
+/* /* /* Third Comment */
+////// Fourth Comment  
+/*
+Fourth is The Wrong Comment
+*/
